969f4d7a331bc8ccbe5c738b403cd30f6ef1710b
Pierre <pierrechapelet60@gmail.com>
2017-07-19 13:20:15 +0200

    push

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+	private GameObject CurrentWindow;
+
+	private GameObject[] Pages;
+	private int CurrentPage;
+
+	private Transform tf;
+
+	void Start() {
+		tf = transform;
+
+		ShowWindow (GameObject.Find ("Window1").gameObject);
+	}
+
+	public void LoadMain() {
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void ShowWindow(GameObject window) {
+		CurrentWindow = window;
+		CurrentWindow.SetActive (true);
+		var pages = CurrentWindow.transform.Find ("Pages");
+		Pages = new GameObject[pages.childCount];
+
+		for (int i = 0; i < Pages.Length; i++) {
+			Pages [i] = pages.GetChild (i).gameObject;
+			Pages [i].SetActive (false);
+		}
+		Pages [0].SetActive (true);
+		CurrentPage = 0;
+	}
+
+	public void CloseWindow() {
+		CurrentWindow.SetActive (false);
+		CurrentWindow = null;
+	}
+
+	public void NextPage() {
+		if (CurrentPage < Pages.Length - 1) {
+			Pages [CurrentPage].SetActive (false);
+			CurrentPage++;
+			Pages [CurrentPage].SetActive (true);
+		}
+	}
+
+	public void PreviousPage() {
+		if (CurrentPage > 0) {
+			Pages [CurrentPage].SetActive (false);
+			CurrentPage--;
+			Pages [CurrentPage].SetActive (true);
+		}
+	}
+}
