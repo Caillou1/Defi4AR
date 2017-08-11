d4871796ed7cb9088919c57fcb09deb073c72747
Pierre <pierrechapelet60@gmail.com>
2017-08-11 13:48:34 +0200

    push

--- Assets/Scripts/GameObjectEnabler.cs
+++ Assets/Scripts/GameObjectEnabler.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GameObjectEnabler : AAction {
 	public List<GameObject> ToEnable;
 	public List<GameObject> ToDisable;
-	public bool StartOnLaunch;
 
 	public override void StartAction ()
 	{
-		foreach (var o in ToEnable)
+		foreach (var o in ToEnable) {
 			o.SetActive (true);
-		foreach (var o in ToDisable)
+		}
+
+		foreach (var o in ToDisable) {
 			o.SetActive (false);
+		}
 	}
 
 	public override void StopAction ()
