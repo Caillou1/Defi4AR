1bca0303075c8d2546b81dfb9c85009f1303cc05
Caillou <p.chapelet@rubika-edu.com>
2017-08-29 09:50:00 +0200

    p

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour {
+public class Rotator : AAction {
 	public float RotationSpeed;
 	public Vector3 RotationVector;
 
 	private Transform tf;
+	private bool Rotating;
 
 	void Start() {
 		tf = transform;
 	}
 
+	public override void StartAction ()
+	{
+		Rotating = true;	
+	}
+
+	public override void StopAction ()
+	{
+		Rotating = false;
+	}
+
 	void Update() {
-		tf.Rotate (Time.deltaTime * RotationVector * RotationSpeed);
+		if(Rotating)
+			tf.Rotate (Time.deltaTime * RotationVector * RotationSpeed);
 	}
 }
